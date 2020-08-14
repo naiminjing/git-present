0d1dc90a720fe5831fa399230a2166947fff1cc1
Naimin Jing <naimin.jing@merck.com>
2020-08-14 10:29:25 -0400

    Add points

--- plotdata.R
+++ plotdata.R
@@ -3,5 +3,6 @@ library(ggplot2)
 data <- data.frame(x=1:10, y=(1:10)*3)
 
 ggplot(data, aes(x = x, y = y))+
-  geom_line(color='red')
+  geom_line(color='red')+
+  geom_point(size = 3, color = 'red', shape = 15)
 ggsave("example.png", width = 7, height = 5)
