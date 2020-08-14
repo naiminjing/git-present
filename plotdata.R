5753d80c6cfc96cb3445bacb5d44c37d623daa60
Naimin Jing <naimin.jing@merck.com>
2020-08-14 10:52:07 -0400

    change the color back to blue

--- plotdata.R
+++ plotdata.R
@@ -3,7 +3,7 @@ library(ggplot2)
 data <- data.frame(x=1:10, y=(1:10)*3)
 
 ggplot(data, aes(x = x, y = y))+
-  geom_line(color='red')+
+  geom_line(color='blue')+
   geom_point(size = 3, color = 'red', shape = 15)+
   geom_vline(xintercept = 4, linetype=14)+
   scale_x_continuous(breaks=1:10)
